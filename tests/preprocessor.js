017cfd7e96da295b0010ea41ec9dbc018d9d1d98
ersenal <aliersenal@gmail.com>
2015-02-16 02:02:03 +0000

    Don't perform Babel transform on node_modules

--- tests/preprocessor.js
+++ tests/preprocessor.js
@@ -2,7 +2,7 @@ var Babel = require("babel-core");
 
 module.exports = {
   process: function(src, file) {
-    if(file.match(/\.jsx?$/)) {
+    if(file.indexOf("node_modules") === -1 && file.match(/\.jsx?$/)) {
       return Babel.transform(src, {filename: file}).code;
     } else if(file.match(/\.css$/)) {
       return '';
