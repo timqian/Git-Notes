f5f722d2b3d2bce6d8e8828bd7fc8d496905546f
ersenal <aliersenal@gmail.com>
2015-02-18 16:23:10 +0000

    Dev hot reload support for CSS

--- tasks/webpack/webpack.config.js
+++ tasks/webpack/webpack.config.js
@@ -13,22 +13,25 @@ var common = {
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /src\/.*\.jsx?$/,
         exclude: /node_modules/,
         loaders: ['babel-loader']
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       }
     ]
-  },
-  plugins: [new ExtractTextPlugin("bundle.css")]
+  }
 };
 
 module.exports = {
   release: function() {
     common.debug = false;
+
+    common.module.loaders.push({
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+    });
+
+    common.plugins = common.plugins || [];
+    common.plugins.push(new ExtractTextPlugin("bundle.css"));
     common.plugins.push(new WebPack.optimize.MinChunkSizePlugin({minChunkSize: 10000}));
     common.plugins.push(new WebPack.optimize.LimitChunkCountPlugin({maxChunks: 15}));
     common.plugins.push(new WebPack.optimize.DedupePlugin());
@@ -51,7 +54,14 @@ module.exports = {
     common.debug = true;
     common.entry.unshift('webpack/hot/only-dev-server');
     common.entry.unshift('webpack-dev-server/client?http://0.0.0.0:3000'); // WebpackDevServer host and port
+
     common.module.loaders[0].loaders.unshift('react-hot');
+    common.module.loaders.push({
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
+    });
+
+    common.plugins = common.plugins || [];
     common.plugins.push(new WebPack.HotModuleReplacementPlugin());
     common.plugins.push(new WebPack.NoErrorsPlugin());
 
